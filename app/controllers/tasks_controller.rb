a92c092884f237aa92d71409c12d52e1eba4cbe3
yuuki.tamura <fxxk.the.rock@gmail.com>
2022-05-02 06:10:20 +0000

    カラムの追加やり直し

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -56,6 +56,6 @@ class TasksController < ApplicationController
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_053300) do
+ActiveRecord::Schema.define(version: 2022_05_02_060118) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "status"
   end
 
 end
