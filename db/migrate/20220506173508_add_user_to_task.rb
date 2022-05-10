8a14526685ed6a49ed049ebb89f3802aed55e303
yuuki.tamura <fxxk.the.rock@gmail.com>
2022-05-10 09:55:23 +0000

    テーブルの修正

--- db/migrate/20220506173508_add_user_to_task.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToTask < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :tasks, :user, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_06_173508) do
+ActiveRecord::Schema.define(version: 2022_07_30_053300) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "status"
     t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
